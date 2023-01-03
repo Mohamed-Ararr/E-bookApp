e39ac69b783e9576206f5ef69141eeb9dd04adc1
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-01-03 12:28:44 +0000

    add project components

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
