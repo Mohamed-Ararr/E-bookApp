6b277e8464a6a550e3afe9299ed9e1be54823b52
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-01-03 13:27:19 +0000

    add splashViewBody to splashView widget

--- lib/features/splash/presentation/views/splashView.dart
+++ lib/features/splash/presentation/views/splashView.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
@@ -5,6 +7,6 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold();
   }
 }
