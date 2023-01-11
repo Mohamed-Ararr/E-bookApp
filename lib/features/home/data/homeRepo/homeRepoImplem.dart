a1b6cf102fe392b7e065e67938df5c125708afd6
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-01-11 12:17:38 +0000

    add fetchFeaturedBooks method

--- lib/features/home/data/homeRepo/homeRepoImplem.dart
+++ lib/features/home/data/homeRepo/homeRepoImplem.dart
@@ -29,7 +29,20 @@ class HomeRepoImplem implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: "volumes?q=subject:programming&Filtering=free-ebook");
+      List<BookModel> newestBooks = [];
+      for (var item in data["items"]) {
+        newestBooks.add(BookModel.fromJson(item));
+      }
+      return right(newestBooks);
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
