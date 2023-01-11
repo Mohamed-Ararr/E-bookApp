a0786f7a8270fc56e6f5a75e0d401ef3ae9fcb9c
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-01-11 13:37:48 +0000

    add fetchFeaturedbooks cubit method

--- lib/features/home/homePresentation/bloc manager/featuredBooksCubit/featuredBooksCubit.dart
+++ lib/features/home/homePresentation/bloc manager/featuredBooksCubit/featuredBooksCubit.dart
@@ -1,8 +1,24 @@
 // ignore_for_file: file_names
 
+import 'package:bookstore/features/home/data/homeRepo/homeRepo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'featuredbooksStates.dart';
 
 class FeaturedBooksCubit extends Cubit {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(FeaturedBooksFailure(failure.errorMsg));
+      },
+      (books) {
+        emit(FeaturedBooksSuccess(books));
+      },
+    );
+  }
 }
